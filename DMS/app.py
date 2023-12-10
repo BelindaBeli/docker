73efecbdb75591f57b9950c514cfaab132c89202
Belinda Fritsch <belinda.fritsch@gmail.com>
2023-12-10 09:57:22 +0100

    commit1

---
+++ DMS/app.py
@@ -0,0 +1,31 @@
+from flask import Flask, render_template, request, redirect, url_for, send_from_directory
+import os
+
+app = Flask(__name__)
+UPLOAD_FOLDER = os.path.join(os.getcwd(), 'uploads')  # Absoluter Pfad zum Upload-Verzeichnis
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+@app.route('/')
+def index():
+    files = os.listdir(app.config['UPLOAD_FOLDER'])
+    return render_template('index.html', files=files)
+
+@app.route('/upload', methods=['POST'])
+def upload_file():
+    if 'file' not in request.files:
+        return redirect(request.url)
+    file = request.files['file']
+    if file.filename == '':
+        return redirect(request.url)
+    if file:
+        file.save(os.path.join(app.config['UPLOAD_FOLDER'], file.filename))
+        return redirect(url_for('index'))
+
+@app.route('/uploads/<filename>')
+def uploaded_file(filename):
+    return send_from_directory(app.config['UPLOAD_FOLDER'], filename)
+
+if __name__ == '__main__':
+    if not os.path.exists(app.config['UPLOAD_FOLDER']):
+        os.makedirs(app.config['UPLOAD_FOLDER'])
+    app.run(debug=True, host='0.0.0.0')
